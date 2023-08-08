36c70009ccb96b7782aeb867c1cf42c15ede35c4
ayoub <ayoub-ok147@hotmail.fr>
2023-08-08 14:32:23 +0100

    upload

---
+++ logic.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+const createAccountLink = document.getElementById('createAccountLink');
+const loginLink = document.getElementById('loginLink');
+
+createAccountLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  loginForm.style.display = 'none';
+  signupForm.style.display = 'block';
+});
+
+loginLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  signupForm.style.display = 'none';
+  loginForm.style.display = 'block';
+});
